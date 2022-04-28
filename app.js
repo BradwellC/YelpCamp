d5f8a2aea2f9754457c55e38fca04260a2e0f2b2
BradwellC <59516796+BradwellC@users.noreply.github.com>
2022-04-28 11:06:50 +0100

    v1

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection Error'));
 db.once('open', () => {
     console.log('Database Connected');
-})
+});
 
 // GkUoRYP9wBU2smxu
 
@@ -24,16 +24,35 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get('/', (req, res) => {
     res.render('home.ejs');
+});
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const campNew = new Campground(req.body.campground);
+    await campNew.save();
+
 })
 
-app.get('/campground', async (req, res) => {
-    const campground = new Campground({ title: 'My Garden' });
-    await campground.save();
-    res.send(campground);
-})
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+});
+
+
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
-})
+});
