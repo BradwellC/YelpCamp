506e00c1e962f26787fbd1d322959eb3160aa327
BradwellC <59516796+BradwellC@users.noreply.github.com>
2022-05-31 16:05:20 +0100

    v3

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const engine = require('ejs-mate');
 
+const { campgroundSchema } = require('./schemas');
+
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
@@ -35,6 +37,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
 
+
+const validateCampground = (req, res, next) => {
+
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+
+
 app.get('/', (req, res) => {
     res.render('home.ejs');
 });
@@ -48,8 +65,8 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if (!req.body.campground) throw new ExpressError('Invalid Campground Data!', 400);
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data!', 400);
 
     const campground = new Campground(req.body.campground);
     await campground.save();
@@ -67,7 +84,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/update', { campground });
 }));
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
 
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
@@ -81,15 +98,24 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }))
 
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+app.post('/signup', (req, res) => {
+    res.render('campgrounds/index');
+})
+
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
 })
 
+
 app.use((err, req, res, next) => {
 
-    const { statusCode = 500, message = 'Something went wrong' } = err;
-
-    res.status(statusCode).send(message);
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Somethig went wrong with your search!'
+    res.status(statusCode).render('error.ejs', { err });
 })
 
 app.listen(3000, () => {
--- public/js/base.js
+++ public/js/base.js
@@ -1,12 +1,57 @@
+// Modal JS
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
-
 const modalClose = document.querySelector('.modal-close');
 const planBtn = document.querySelectorAll('#modal-btn');
 
+// Navbar JS
 const navbarToggle = document.querySelector('.navbar-toggle');
 const mobileNav = document.querySelector('.mobile-navbar');
 
+// Form JS
+const usernameEl = document.querySelector('#username');
+const emailEl = document.querySelector('#email');
+const passwordEl = document.querySelector('#password');
+const confirmPasswordEl = document.querySelector('#password-confirm');
+const form = document.querySelector('#form');
+
+// Reuseable functions
+const required = value => value === '' ? false : true;
+const between = (length, min, max) => length < min || length > max ? false : true;
+const emailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+const securePassword = (password) => {
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return re.test(password);
+};
+
+const showError = (input, message) => {
+    const formGroup = input.parentElement;
+
+    formGroup.classList.remove('success');
+    formGroup.classList.add('error');
+
+    const error = formGroup.querySelector('small');
+    error.textContent = message;
+}
+
+const showSuccess = (input) => {
+    // get the form-field element
+    const formGroup = input.parentElement;
+
+    // remove the error class
+    formGroup.classList.remove('error');
+    formGroup.classList.add('success');
+
+    // hide the error message
+    const error = formGroup.querySelector('small');
+    error.textContent = '';
+}
+
+
+// Main JS
 for (var i = 0; i < planBtn.length; i++) {
     planBtn[i].addEventListener('click', function () {
         modal.classList.add('open');
@@ -78,4 +123,145 @@ $(document).ready(function () {
             });
         } // End if
     });
+});
+
+// Alerts
+var close = document.getElementsByClassName("closebtn");
+var i;
+
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.opacity = "0";
+        setTimeout(function () { div.style.display = "none"; }, 600);
+    }
+}
+
+// Form JS
+const usernameCheck = () => {
+    let vaild = false;
+    const min = 3,
+        max = 25;
+
+    const username = usernameEl.value.trim();
+
+    if (!required(username)) {
+        showError(usernameEl, 'Username cannot be blank');
+    } else if (!between(username.length, min, max)) {
+        showError(usernameEl, `Username must be between ${min} and ${max}`);
+    } else {
+        showSuccess(usernameEl);
+        vaild = true;
+    }
+
+    return vaild;
+}
+
+const passwordCheck = () => {
+    let valid = false;
+
+    const password = passwordEl.value.trim();
+
+    if (!required(password)) {
+        showError(passwordEl, 'Pasword cannot be blank!');
+    } else if (!securePassword(password)) {
+        showError(passwordEl, 'Password must has at least 8 characters that include at least 1 lowercase character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)');
+    } else {
+        showSuccess(passwordEl);
+        valid = true;
+    }
+
+    return valid;
+}
+
+const confirmPasswordCheck = () => {
+    let valid = false;
+    // check confirm password
+    const confirmPassword = confirmPasswordEl.value.trim();
+    const password = passwordEl.value.trim();
+
+    if (!required(confirmPassword)) {
+        showError(confirmPasswordEl, 'Please enter the password again');
+    } else if (password !== confirmPassword) {
+        showError(confirmPasswordEl, 'Confirm password does not match');
+    } else {
+        showSuccess(confirmPasswordEl);
+        valid = true;
+    }
+
+    return valid;
+};
+
+form.addEventListener('submit', (e) => {
+    // Prevents form from submitting
+    e.preventDefault();
+
+    form.addEventListener('submit', function (e) {
+        // prevent the form from submitting
+        e.preventDefault();
+
+        // validate forms
+        let isUsernameValid = usernameCheck(),
+            isEmailValid = emailCheck(),
+            isPasswordValid = passwordCheck(),
+            isConfirmPasswordValid = confirmPasswordCheck();
+
+        let isFormValid = isUsernameValid &&
+            isEmailValid &&
+            isPasswordValid &&
+            isConfirmPasswordValid;
+
+        // submit to the server if the form is valid
+        if (isFormValid) {
+
+        }
+    });
+});
+
+const debounce = (fn, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+        // cancel the previous timer
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        // setup a new timer
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args)
+        }, delay);
+    };
+};
+
+form.addEventListener('input', debounce(function (e) {
+    switch (e.target.id) {
+        case 'username':
+            usernameCheck();
+            break;
+        case 'email':
+            emailCheck();
+            break;
+        case 'password':
+            passwordCheck();
+            break;
+        case 'confirm-password':
+            confirmPasswordCheck();
+            break;
+    }
+}));
+
+form.addEventListener('input', function (e) {
+    switch (e.target.id) {
+        case 'username':
+            usernameCheck();
+            break;
+        case 'email':
+            emailCheck();
+            break;
+        case 'password':
+            passwordCheck();
+            break;
+        case 'confirm-password':
+            confirmPasswordCheck();
+            break;
+    }
 });
